d94c04ba5e5b33eb7cc49f51fe9ceed6732f6a0b
Aux5e <eduardofreire@outlook.com>
2021-12-16 16:50:37 -0300

    lista de magias de classes

---
+++ js/lista-de-magias-classes.js
@@ -0,0 +1,85 @@
+async function loadIntoTable(url, table) {
+  const tableBody = document.getElementById("tbody-lista-de-magias");
+
+  // pegar nome da página para filtrar classe
+  var path = window.location.pathname;
+  var page = path.split("/").pop();
+  var pageName = page.split(".")[0]; // <-- added this line
+
+  await $.getJSON(url, function (magias) {
+    const magiasValues = Object.keys(magias.data);
+    const siteUrl = "https://aux5e.github.io";
+    const magiasArray1 = Object.values(magias.data);
+    const magiasLenght = Object.keys(magias.data).length;
+
+    let magiasArray = [];
+    for (let i = 0; i < magiasLenght; i++) {
+      if (magiasArray1[i][pageName] == true) {
+        magiasArray.push(magiasArray1[i]);
+      }
+    }
+
+    const magiasArrayLenght = magiasArray.length;
+
+    // Limpa o corpo da tabela
+    tableBody.innerHTML = "";
+
+    // Popular o corpo da tabela
+    var r = [];
+    var j = -1;
+    for (var row = 0; row < magiasArrayLenght; row++) {
+      r[++j] = "<tr onclick=";
+      r[++j] = 'window.location="';
+      r[++j] = siteUrl;
+      r[++j] = "/magias.html?nome=";
+      r[++j] = magiasArray1[row];
+      r[++j] =
+        '"><th scope="row" class="text-center align-middle magiaCirculoCol">';
+      if (magiasArray[row]["circulo"] == 0) {
+        r[++j] =
+          '<span style="visibility:hidden;">0</span>Truque<span style="visibility:hidden;">0</span>';
+      } else {
+        r[++j] = magiasArray[row]["circulo"];
+        r[++j] = "º";
+      }
+      r[++j] = "</th>";
+      r[++j] = '<td><div class="magiaNomeRow1">';
+      r[++j] = magiasArray[row]["nome"];
+      if (magiasArray[row]["concentracao"] == true) {
+        r[++j] = '<span class="concentSpan"></span>';
+      }
+      r[++j] = '</div><div class="magiaNomeRow2">';
+      r[++j] = magiasArray[row]["escola"] + " • " + magiasArray[row]["componentes"];
+      r[++j] = "</div></td>";
+      r[++j] = '<td class="align-middle magiaInglesCol">';
+      r[++j] = magiasArray[row]["nome_ingles"];
+      r[++j] = '</td><td class="align-middle magiaOutrasCol">';
+      r[++j] = magiasArray[row]["duracao"];
+      r[++j] = '</td><td class="align-middle magiaOutrasCol">';
+      r[++j] = magiasArray[row]["alcance"];
+      r[++j] = '</td><td class="text-center align-middle magiaOutrasCol">';
+      r[++j] = magiasArray[row]["fonte"];
+      r[++j] = "</td></tr>";
+    }
+
+    $(tableBody).html(r.join(""));
+  });
+}
+
+$(document).ready(async function () {
+  await loadIntoTable(
+    "/data/magias.json",
+    document.getElementById("table-lista-de-magias")
+  );
+
+  $(document).ready(function () {
+    $("#table-lista-de-magias").DataTable({
+      responsive: true,
+      language: {
+        url: "/data/dataTables.ptbr.json",
+      },
+      pageLength: 25,
+      paging: true,
+    });
+  });
+});
---
+++ js/lista-de-magias.js
@@ -0,0 +1,71 @@
+async function loadIntoTable(url, table) {
+  const tableBody = document.getElementById("tbody-lista-de-magias");
+
+  await $.getJSON(url, function (magias) {
+    const magiasValues = Object.keys(magias.data);
+    const siteUrl = "https://aux5e.github.io";
+    const magiasArray = Object.values(magias.data);
+    const magiasLenght = Object.keys(magias.data).length;
+
+    // Limpa o corpo da tabela
+    tableBody.innerHTML = "";
+
+    // Popular o corpo da tabela
+    var r = [];
+    var j = -1;
+    for (var row = 0; row < magiasLenght; row++) {
+      r[++j] = "<tr onclick=";
+      r[++j] = 'window.location="';
+      r[++j] = siteUrl;
+      r[++j] = "/magias.html?nome=";
+      r[++j] = magiasValues[row];
+      r[++j] =
+        '"><th scope="row" class="text-center align-middle magiaCirculoCol">';
+      if (magiasArray[row].circulo == 0) {
+        r[++j] =
+          '<span style="visibility:hidden;">0</span>Truque<span style="visibility:hidden;">0</span>';
+      } else {
+        r[++j] = magiasArray[row].circulo;
+        r[++j] = "º";
+      }
+      r[++j] = "</th>";
+      r[++j] = '<td><div class="magiaNomeRow1">';
+      r[++j] = magiasArray[row].nome;
+      if (magiasArray[row].concentracao == true) {
+        r[++j] = '<span class="concentSpan"></span>';
+      }
+      r[++j] = '</div><div class="magiaNomeRow2">';
+      r[++j] = magiasArray[row].escola + " • " + magiasArray[row].componentes;
+      r[++j] = "</div></td>";
+      r[++j] = '<td class="align-middle magiaInglesCol">';
+      r[++j] = magiasArray[row].nome_ingles;
+      r[++j] = '</td><td class="align-middle magiaOutrasCol">';
+      r[++j] = magiasArray[row].duracao;
+      r[++j] = '</td><td class="align-middle magiaOutrasCol">';
+      r[++j] = magiasArray[row].alcance;
+      r[++j] = '</td><td class="text-center align-middle magiaOutrasCol">';
+      r[++j] = magiasArray[row].fonte;
+      r[++j] = "</td></tr>";
+    }
+
+    $(tableBody).html(r.join(""));
+  });
+}
+
+$(document).ready(async function () {
+  await loadIntoTable(
+    "/data/magias.json",
+    document.getElementById("table-lista-de-magias")
+  );
+
+  $(document).ready(function () {
+    $("#table-lista-de-magias").DataTable({
+      responsive: true,
+      language: {
+        url: "/data/dataTables.ptbr.json",
+      },
+      pageLength: 25,
+      paging: true,
+    });
+  });
+});
