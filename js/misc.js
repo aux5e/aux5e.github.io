5e22fae50fa98673c028d7eb665058546041f390
Aux5e <eduardofreire@outlook.com>
2022-02-01 13:30:56 -0300

    Update misc.js

--- js/misc.js
+++ js/misc.js
@@ -29,41 +29,42 @@ $(document).ready(function () {
     });
 });
 
+// slugify remove acentos, caracteres especiais e substitui espaços com hífens
 function slugify(value) {
     return value.replace(/[àáâãäå]/g, "a").replace(/ç/g,"c").replace(/[èéêë]/g,"e").replace(/[ìíîï]/g,"i").replace(/[òóôõö]/g,"o").replace(/[ùúûü]/g,"u").replace(/ /g, "-");
 }
 
-const linkTypes = {
+/** Replace regular expression ex. {@spell} {@condition} etc */
+const EXPRESSION_TYPE = {
     "spell": "magias",
     "condition": "condicoes",
     "creature": "criaturas"
 }
 
-/** Replace @ spell */
 window.spellToLink = function spellToLink(datas) {
     return datas.map(data => {
         data.subclassFeatures = data.subclassFeatures.map(features => {
             features.entries = features.entries.map(entry => {
                 if (typeof entry === 'string') {
                     //console.log(entry);
-                    const regex = new RegExp("{@(?<type>\\w+)\\s+(?<name>.+?)}", 'gi');
-                    const regexMatch = entry.match(regex);
-                    if (regexMatch) {
+                    const REGEX = new RegExp("{@(?<type>\\w+)\\s+(?<name>.+?)}", 'gi');
+                    const REGEX_MATCH = entry.match(REGEX);
+                    if (REGEX_MATCH) {
                         let type, name = null;
-                        regexMatch.forEach(value => {
-                            const group = value.match(/{@(?<type>\w+)\s+(?<name>.+?)}/i).groups;
-                            name = group.name;
-                            type = group.type;
-                            slugId = slugify(group.name);
+                        REGEX_MATCH.forEach(value => {
+                            const GROUP = value.match(/{@(?<type>\w+)\s+(?<name>.+?)}/i).groups;
+                            name = GROUP.name;
+                            type = GROUP.type;
+                            slugId = slugify(GROUP.name);
 
                             if (type === 'spell') {
-                                var link = `<a href="/${linkTypes[type]}.html?nome=${slugId}" class="${linkTypes[type]}-hover ${linkTypes[type]}-tooltip" id="${slugId}" title="Carregando...">${name}</a>`;
+                                var link = `<a href="/${EXPRESSION_TYPE[type]}.html?nome=${slugId}" class="${EXPRESSION_TYPE[type]}-hover ${EXPRESSION_TYPE[type]}-tooltip" id="${slugId}" title="Carregando...">${name}</a>`;
                                 entry = entry.replace(value, link);
                             } else if (type === 'condition') {
-                                var link = `<a href="javascript:void(0)" class="${linkTypes[type]}-hover ${linkTypes[type]}-tooltip" id="${slugId}" title="Carregando...">${name}</a>`;
+                                var link = `<a href="javascript:void(0)" class="${EXPRESSION_TYPE[type]}-hover ${EXPRESSION_TYPE[type]}-tooltip" id="${slugId}" title="Carregando...">${name}</a>`;
                                 entry = entry.replace(value, link);
                             } else if (type === 'creature') {
-                                var link = `<span class="${linkTypes[type]}-hover ${linkTypes[type]}-tooltip" id="${slugId}">${name}</span>`;
+                                var link = `<span class="${EXPRESSION_TYPE[type]}-hover ${EXPRESSION_TYPE[type]}-tooltip" id="${slugId}">${name}</span>`;
                                 entry = entry.replace(value, link);
                             }
                         })                        
@@ -79,18 +80,3 @@ window.spellToLink = function spellToLink(datas) {
         return data;
     })
 }
-
-//// Get the container element
-//var btnContainer = document.getElementById("nav-menu");
-
-//// Get all buttons with class="nav-link" inside the container
-//var btns = btnContainer.getElementsByClassName("nav-item");
-
-//// Loop through the buttons and add the active class to the current/clicked button
-//for (var i = 0; i < btns.length; i++) {
-//    btns[i].addEventListener("click", function () {
-//        var current = document.getElementsByClassName("active");
-//        current[0].className = current[0].className.replace(" active", "");
-//        this.className += " active";
-//    });
-//}
